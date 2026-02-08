5b290177a9ca347dcec1c57a9ed52540ab0894c0
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-02-08 10:37:33 +0530

    har har har

---
+++ hw.py
@@ -0,0 +1,8 @@
+amount_total = 40
+amount_payed = 29
+amount_left = amount_total - amount_payed
+if False:
+    print("The total amount is:", amount_total)
+else:
+    pass
+print("The amount left is:", amount_left)
